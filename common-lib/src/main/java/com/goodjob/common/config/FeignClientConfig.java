70d83488ba361ab61a1c6677d372a15c993e870d
TanVX <tanvx.training@gmail.com>
2025-03-22 00:24:03 +0700

    call internal in company service

--- common-lib/src/main/java/com/goodjob/common/config/FeignClientConfig.java
+++ common-lib/src/main/java/com/goodjob/common/config/FeignClientConfig.java
@@ -51,29 +51,42 @@ public class FeignClientConfig {
         return requestTemplate -> {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             if (authentication != null && authentication.isAuthenticated()) {
-                String username = authentication.getName();
-                if (username != null) {
-                    requestTemplate.header("X-Auth-Username", username);
-                }
-
-                String roles = authentication.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .filter(authority -> authority.startsWith("ROLE_"))
-                        .collect(Collectors.joining(","));
-                String permissions = authentication.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .filter(authority -> !authority.startsWith("ROLE_"))
-                        .collect(Collectors.joining(","));
-
-                if (!roles.isEmpty()) {
-                    requestTemplate.header("X-Auth-Roles", roles);
-                }
-                if (!permissions.isEmpty()) {
-                    requestTemplate.header("X-Auth-Permissions", permissions);
+                Object credentials = authentication.getCredentials();
+                if (credentials instanceof String jwt) {
+                    requestTemplate.header("Authorization", "Bearer " + jwt);
                 }
             }
         };
     }
+
+//    @Bean
+//    public RequestInterceptor requestInterceptor() {
+//        return requestTemplate -> {
+//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//            if (authentication != null && authentication.isAuthenticated()) {
+//                String username = authentication.getName();
+//                if (username != null) {
+//                    requestTemplate.header("X-Auth-Username", username);
+//                }
+//
+//                String roles = authentication.getAuthorities().stream()
+//                        .map(GrantedAuthority::getAuthority)
+//                        .filter(authority -> authority.startsWith("ROLE_"))
+//                        .collect(Collectors.joining(","));
+//                String permissions = authentication.getAuthorities().stream()
+//                        .map(GrantedAuthority::getAuthority)
+//                        .filter(authority -> !authority.startsWith("ROLE_"))
+//                        .collect(Collectors.joining(","));
+//
+//                if (!roles.isEmpty()) {
+//                    requestTemplate.header("X-Auth-Roles", roles);
+//                }
+//                if (!permissions.isEmpty()) {
+//                    requestTemplate.header("X-Auth-Permissions", permissions);
+//                }
+//            }
+//        };
+//    }
 
     /**
      * Custom error decoder for Feign clients.
