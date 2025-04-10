f6c8fe28f6227e8026ad5e60d6f5ffa5c9b3fada
tanvx <tanvx.training@gmail.com>
2025-04-10 23:43:00 +0700

    implement kafka in job and posting

---
+++ job-service/src/main/java/com/goodjob/job/config/KafkaConfig.java
@@ -0,0 +1,38 @@
+package com.goodjob.job.config;
+
+import com.goodjob.job.common.dto.KafkaConstants;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic postingTopic() {
+        return TopicBuilder.name(KafkaConstants.POSTING_JOB_TOPIC)
+                .partitions(6)
+                .replicas(2)
+                .configs(Map.of(
+                        "retention.ms", "604800000", // Giữ message trong 7 ngày
+                        "segment.bytes", "1073741824" // Segment size 1GB
+                ))
+                .build();
+    }
+
+    // Tạo topic "un-posting"
+    @Bean
+    public NewTopic unPostingTopic() {
+        return TopicBuilder.name(KafkaConstants.UN_POSTING_JOB_TOPIC)
+                .partitions(6)
+                .replicas(2)
+                .configs(Map.of(
+                        "retention.ms", "604800000", // Giữ message trong 7 ngày
+                        "segment.bytes", "1073741824" // Segment size 1GB
+                ))
+                .build();
+    }
+}
